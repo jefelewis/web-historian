161a4657a7041c48c09d92ff869f532e8d34ad96
Hack Reactor Student <student@hackreactor.com>
2018-02-07 15:17:02 -0800

    HTML, ; will return to CSS fix

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,6 +5,21 @@ var archive = require('../helpers/archive-helpers');
 var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
+// exports.css = function (req, res) {
+  // if (req.url.indexOf('.css')) {
+//     res.writeHead(200, {'Content-type': 'text/css'});
+//     var css = path.resolve(__dirname, './public/styles.css');
+//     fs.readFile(css, function (err, data) {
+//       if (err) {
+//         throw err;
+//       } else {
+//         res.write(data);
+//         res.end();
+//       }
+//     })
+//   }
+// };
+
 exports.handleRequest = function (req, res) {
   // console.log(req.method);
 
@@ -15,32 +30,33 @@ exports.handleRequest = function (req, res) {
   //   return;
   // }
 
-  // CSS
+  function getSite(res, path) {
+    fs.readFile(path, function (error, data) {
+      if(error){
+        throw 'BUMMER! ' + error;
+      }
+      else {
+        res.write(data);
+        // res.end();
+      }
+    });
+  }
 
+  // If Request Method is GET
   if (req.method === 'GET') {
-    res.writeHead(200, httpHelpers.headers);
-    console.log('YOU ARE HERE ' + req);
-    
-    if (req.url === '/styles.css') {
-      res.writeHead(200, {'Content-type': 'text/css'});
-    }
-
     if (req.url === '/') {
+      res.writeHead(200, {'Content-type': 'text/html'});
       var site = path.resolve(__dirname, './public/index.html');
       fs.readFile(site, function (error, data) {
-        if(error){
-          throw 'BUMMER! ' + error;
-        }
-        else {
-          console.log('SITE is: ' + site);
-          console.log(data.toString());
-          res.write(data);
-          res.end();
-        }
-      });
+          if(error){
+            throw 'BUMMER! ' + error;
+          }
+          else {
+            res.write(data);
+            res.end();
+          }
+        });
     }
-
-    // res.end('SUCESSFUL GET');
   } else if (req.method === 'POST'){
     res.writeHead(200, httpHelpers.headers);
     res.end('SUCESSFUL POST')
@@ -49,6 +65,6 @@ exports.handleRequest = function (req, res) {
     
     res.end('SUCESSFUL OPTIONS')
   }
-  
+
   // res.end(archive.paths.list);
 };
