097876cd5621160b1322adf57f6c1627abde9074
Hack Reactor Student <student@hackreactor.com>
2018-02-07 13:40:48 -0800

    Render HTML without CSS

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,47 @@
 var path = require('path');
+// var urlMod = require('url');
+var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // console.log(req.method);
+
+  // if (req.url === '/favicon.ico') {
+  //   res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+  //   res.end();
+  //   console.log('favicon requested');
+  //   return;
+  // }
+
+  if (req.method === 'GET') {
+    res.writeHead(200, httpHelpers.headers);
+    
+    if (req.url === '/') {
+    console.log('YOU ARE HERE ' + req.url);
+      var site = path.resolve(__dirname, './public/index.html');
+      fs.readFile(site, function (error, data) {
+        if(error){
+          throw 'BUMMER! ' + error;
+        }
+        else {
+          // console.log(data.toString());
+          res.write(data);
+          res.end();
+        }
+      });
+    }
+
+    // res.end('SUCESSFUL GET');
+  } else if (req.method === 'POST'){
+    res.writeHead(200, httpHelpers.headers);
+    res.end('SUCESSFUL POST')
+  } else if (req.method === 'OPTIONS') {
+    res.writeHead(200, httpHelpers.headers);
+    
+    res.end('SUCESSFUL OPTIONS')
+  }
+  
+  // res.end(archive.paths.list);
 };
